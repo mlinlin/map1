84f678ba775c0d6c5c2e92508f984de485fdb337
mlinlin <38514867+mlinlin@users.noreply.github.com>
2019-09-02 20:43:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ const svg = d3.select("svg");
 
 const path = d3.geoPath();
 
-d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
-  if (error) throw error;
-
+d3.json("https://d3js.org/us-10m.v1.json").then(function(us) {
   svg.append("g")
       .attr("class", "counties")
     .selectAll("path")
@@ -17,6 +15,6 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
 
   svg.append("path")
       .attr("class", "county-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
+      .attr("d", path(topojson.mesh(us, us.objects.counties).then(function(a, b) { return a !== b; })));
 });
 }
